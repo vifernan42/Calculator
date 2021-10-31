659772c4101344c50b2c331e9ff1b36079779a77
vifernan42 <vifernan@student.42madrid.com>
2021-10-31 02:04:22 +0100

    final

--- calc.js
+++ calc.js
@@ -20,37 +20,80 @@ var d = 0;
 var l = 0;
 var p = 0;
 var eq = 0;
+var po = 0;
+var per = 0;
 
-function clicking()
-{
-	document.getElementsByClassName('drill_cursor')[0].addEventListener('click', function (event) {
-		document.getElementbyClassName("f").style.backgroundColor="blue";
-	});
-}
 function principal(value){
-	if (m > 0 || d > 0 || l > 0 || p > 0)
+	if (m > 0 || d > 0 || l > 0 || p > 0 || per > 0)
 	{
-		if (flag2 != 0)
+		if (flag2 != 0 && po == 0)
 			global2 *= 10;
+		if (flag2 != 0 && po > 0)
+		{
+			for (let i = 0; i < po; i++) {
+				value /= 10;
+				console.log("VALUE" + value);
+			}
+			po++;
+		}
 		flag2++;
 		global2 += value;
+		if (po > 0)
+			document.getElementById("principal").value = Number(global2).toFixed(po - 1);
+		else
+			document.getElementById("principal").value = global2;
+	}
+	else
+	{
+		if (flag != 0 && po == 0)
+			global *= 10;
+		if (flag != 0 && po > 0)
+		{
+			for (let i = 0; i < po; i++) {
+				value /= 10;
+				console.log("VALUE" + value);
+			}
+			po++;
+		}
+		global += value;
+		flag++;
+		if (po > 0)
+			document.getElementById("principal").value = Number(global).toFixed(po - 1);
+		else
+			document.getElementById("principal").value = global;
+	}
+	console.log("PO:" + po);
+	console.log("GLOBAL:" + global);
+}
+
+function masme(value){
+	if (m > 0 || d > 0 || l > 0 || p > 0 || per > 0)
+	{
+		global2 *= -1;
 		document.getElementById("principal").value = global2;
 	}
 	else
 	{
-		if (flag != 0)
-			global *= 10;
-		global += value;
+		global *= -1;
 		document.getElementById("principal").value = global;
-		flag++;
 	}
-	console.log(global);
+}
+
+function perc(value){
+	p = 0;
+	m = 0;
+	d = 0;
+	po = 0;
+	l = 0;
+	per++;
 }
 
 function menus(value){
 	p = 0;
 	m = 0;
 	d = 0;
+	po = 0;
+	per = 0;
 	l++;
 	if (l == 2)
 	{
@@ -65,6 +108,8 @@ function plus(value){
 	l = 0;
 	m = 0;
 	d = 0;
+	po = 0;
+	per = 0;
 	p++;
 	if (p == 2)
 	{
@@ -79,6 +124,8 @@ function divi(value){
 	p = 0;
 	l = 0;
 	m = 0;
+	po = 0;
+	per = 0;
 	d++;
 	if (d == 2)
 	{
@@ -93,6 +140,8 @@ function multi(value){
 	d = 0;
 	l = 0;
 	p = 0;
+	po = 0;
+	per = 0;
 	m++;
 	if (m == 2)
 	{
@@ -103,8 +152,22 @@ function multi(value){
 	}
 }
 
+function point(value)
+{
+	po = 1;
+	document.getElementById("principal").value = global + ".";
+}
+
 function del(){
+	d = 0;
+	l = 0;
+	m = 0;
+	p = 0;
+	po = 0;
+	per = 0;
 	global = 0;
+	global2 = 0; 
+	po = 0;
 	document.getElementById("principal").value = global;
 }
 
@@ -129,7 +192,15 @@ function equal(){
 		result = global - global2;
 		l = 0;
 	}
+	if (per > 0)
+	{
+		console.log("2---" + global2);
+		console.log("1---" + global);
+		result = ((global2 * global) / 100).toFixed(2);
+		per = 0;
+	}
 	global = result;
 	global2 = 0;
+	po = 0;
 	document.getElementById("principal").value = result;
 }
